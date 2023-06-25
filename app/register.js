fde55a8c45eaa8cf7fc465d9cd8930bd8757914b
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-25 20:24:47 -0300

    feat: add function create task, update style class

--- app/register.js
+++ app/register.js
@@ -4,40 +4,50 @@ import { MaterialIcons } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
 
 import { Header } from '../components/Header/Header'
+import { useTaskContext } from '../hooks/taskContext'
 
-import { register } from './styles/index'
 import THEME from '../theme'
+import { styles } from './styles/register'
 
 export default function Register() {
+  const [description, setDescription] = React.useState(null)
+  const { createTask } = useTaskContext()
   const router = useRouter()
 
   return (
-    <View style={register.container}>
-      <View style={register.header}>
+    <View style={styles.container}>
+      <View style={styles.header}>
         <Header />
 
         <TouchableOpacity
           activeOpacity={0.7}
-          style={register.icon}
+          style={styles.icon}
           onPress={() => router.back()}
         >
           <MaterialIcons name="keyboard-arrow-left" size={40} color="white" />
         </TouchableOpacity>
       </View>
 
-      <Text style={register.text}>Create task</Text>
+      <Text style={styles.text}>Create task</Text>
 
       {/* input text */}
       <View style={{ flex: 1 }}>
         <TextInput
-          style={register.input}
+          maxLength={50}
+          value={description}
+          onChangeText={setDescription}
+          style={styles.input}
           placeholder="Write a task..."
           placeholderTextColor={THEME.COLORS.WHITE}
         />
       </View>
 
-      <TouchableOpacity style={register.button} activeOpacity={0.7}>
-        <Text style={register.textButton}>Save task</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={() => createTask(description)}
+      >
+        <Text style={styles.textButton}>Save task</Text>
       </TouchableOpacity>
     </View>
   )
