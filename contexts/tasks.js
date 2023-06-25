d5c9b569c65456c0e8d73069e45450472c77d45e
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-25 20:15:45 -0300

    feat: create task context, using custom hook

---
+++ contexts/tasks.js
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { useTasks } from '../hooks/tasks'
+
+const TaskContext = React.createContext({
+  tasks: [],
+  createTask: () => { },
+  deleteTask: () => { },
+  updateTask: () => { }
+})
+
+const TaskProvider = ({ children }) => {
+  const { loadTasks, tasks, createTask, deleteTask } = useTasks()
+
+  React.useEffect(() => {
+    loadTasks()
+  }, [])
+
+  return (
+    <TaskContext.Provider value={{ tasks, createTask, deleteTask }}>
+      {children}
+    </TaskContext.Provider>
+  )
+}
+
+export { TaskProvider, TaskContext }
