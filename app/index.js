2c60bd6cdb231be590d0195bceb6e24b7c914c5b
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-25 20:07:41 -0300

    styles: organize imports

--- app/index.js
+++ app/index.js
@@ -3,9 +3,9 @@ import { View } from 'react-native'
 
 import { Header } from '../components/Header/Header'
 import { Listing } from '../components/Listing/Listing'
+import { RegisterTaskButton } from '../components/RegisterTaskButton/RegisterTaskButton'
 
 import { styles } from './styles'
-import { RegisterTaskButton } from '../components/RegisterTaskButton/RegisterTaskButton'
 
 export default function App() {
   return (
