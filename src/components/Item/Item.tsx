be5805f4e35f82168912e72e4dcae909c8c2505d
TiagoM13 <thyagomotha2000@gmail.com>
2023-11-26 22:34:45 -0300

    feat: adding formatting for dates

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -7,13 +7,31 @@ import { ActionButton } from '../ActionButton'
 import { ITask } from '@/entities/task'
 
 import { styles, text } from './styles'
+import { useRouter } from 'expo-router'
 
 interface ItemProps {
   task: ITask
 }
 
 export function Item({ task }: ItemProps) {
+  const router = useRouter()
+
+  const handleTaskUpdateRoute = React.useCallback(() => {
+    router.push({
+      pathname: 'update',
+      params: {
+        id: task.id,
+      },
+    })
+  }, [])
+
   const checked = task.status === 'completed'
+
+  // format dates
+  const currentDate = task.updated_at ? task.updated_at : task.created_at
+  const date = new Date(`${currentDate}`)
+  const format_date = date.toDateString()
+  const hours = date.toLocaleTimeString()
 
   return (
     <View style={styles.container}>
@@ -21,14 +39,20 @@ export function Item({ task }: ItemProps) {
         <Checkbox checked={checked} onCheck={() => console.log()} />
 
         <View style={styles.textContent}>
-          <Text style={styles.time}>{task.created_at}</Text>
+          <Text style={styles.time}>
+            {format_date} - {hours}
+          </Text>
           <Text style={text({ status: task.status }).description}>
             {task.title}
           </Text>
         </View>
       </View>
 
-      <ActionButton actions={{}} />
+      <ActionButton
+        actions={{
+          update: handleTaskUpdateRoute,
+        }}
+      />
     </View>
   )
 }
