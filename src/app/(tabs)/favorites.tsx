f5120e3bf0fa8d7ca204872ee29a1e8f2388e077
TiagoM13 <thyagomotha2000@gmail.com>
2024-01-23 23:11:51 -0300

    feat: add favorites screen

---
+++ src/app/(tabs)/favorites.tsx
@@ -0,0 +1,11 @@
+import { Title, ViewContainer, ViewContent } from '@/styles'
+
+export default function Favorites() {
+  return (
+    <ViewContainer>
+      <ViewContent>
+        <Title>Favorites</Title>
+      </ViewContent>
+    </ViewContainer>
+  )
+}
