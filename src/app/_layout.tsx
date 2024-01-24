9d465fee3754f75be9560c1555e3be8aab67a4d2
TiagoM13 <thyagomotha2000@gmail.com>
2024-01-23 23:19:26 -0300

    feat: add navigation root

---
+++ src/app/_layout.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import Entypo from '@expo/vector-icons/Entypo'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+
+import { ContainerNavigatorRoot } from '@/styles'
+
+SplashScreen.preventAutoHideAsync()
+
+const Layout = () => {
+  const [ready, setReady] = React.useState(false)
+
+  const [fonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  React.useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync(Entypo.font)
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (ready || fonts) {
+      await SplashScreen.hideAsync()
+    }
+  }, [ready, fonts])
+
+  if (!ready && !fonts) {
+    return null
+  }
+
+  return (
+    <ContainerNavigatorRoot onLayout={onLayoutRootView}>
+      <StatusBar style="light" translucent />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
+      </Stack>
+    </ContainerNavigatorRoot>
+  )
+}
+
+export default Layout
